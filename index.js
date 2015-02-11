649f9ea6a30a4fb8b54862cb03e2606a7db56f74
Mike Pennisi <mike@mikepennisi.com>
2015-02-11 18:35:02 -0500

    Restructure and add more assertions

--- index.js
+++ index.js
@@ -1,54 +1,24 @@
 'use strict';
-var assert = require('assert');
-var recast = require('recast');
-
-var assertions = {
-	assertThrows: function(path) {
-		var args = path.parentPath.value.arguments;
-
-		assert.equal(args.length, 2, 'Unexected invocation of assertThrows');
-
-		args.unshift(args.pop());
-		path.value.name = 'assert.throws';
-	},
-	assertSame: function(path) {
-		path.value.name = 'assert.sameValue';
-	}
-};
-
-function transform(code) {
-	var ast = recast.parse(code);
-
-	var visitor = recast.visit(ast, {
-		names: [],
-		visitIdentifier: function(path) {
-			var node = path.value;
-			this.visitor.names.push(node.name);
-			if (assertions.hasOwnProperty(node.name)) {
-				// We could special cases for different function invocations
-				// patterns, but that would probably be an over-optimization in
-				// this context. Throw an error to prompt manual intervention.
-				recast.types.namedTypes.CallExpression.assert(
-					path.parentPath.value
-				);
-
-				assertions[node.name](path);
-			}
-			this.traverse(path);
-		}
-	});
-
-	return recast.print(ast).code;
-}
+var transform = require('./');
 
 var code = [
 	'// Can recast handle ES6?',
 	'var x = (dooDoo) => { dooDoo };',
 	'class MyClass { constructor() {} };',
 	'// Yes!',
-	'assertSame(5001, 5001);',
-	'// Trickier: argument hockey',
+	'assertTrue(!false, "Should be true");',
+	'assertFalse(!true, "Should be false");',
+	'assertNull(void 0, "Should be null");',
+	'assertNotNull(3, "Should not be null");',
+	'assertSame(5000 + 1, 5001, "Should be the same");',
 	'assertThrows(function() { throw new Error(); }, Error);',
+	'assertArrayEquals([1, 1+1, 3], [1, 2, 4-1], "same elements");',
+	'(function() {',
+	'	assertUnreachable("this code should not be executed.");',
+	'});',
+	'',
+	'// This is not perfect, but I think it\'s good enough:',
+	'failedPromise.then(assertUnreachable);'
 ].join('\n');
 
 console.log('before:');
